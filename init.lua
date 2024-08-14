f6356687eabaabfd5ae33edafe72df441a4238a8
robbe <robinjunker04@gmail.com>
2024-08-14 21:09:34 +0200

    removed undo

--- init.lua
+++ init.lua
@@ -2,14 +2,14 @@ vim.g.mapleader = " "
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -17,17 +17,13 @@ require("lazy").setup("plugins")
 require("config")
 
 
--- Windows-spezifische Pfade für die Undo-Dateien
-local undodir = vim.fn.expand("$HOME") .. "/AppData/Local/nvim/undo"
+local undodir = vim.fn.expand("~") .. "/.config/nvim/undo"
 
--- Stelle sicher, dass das Verzeichnis für die Undo-Dateien existiert
 if vim.fn.isdirectory(undodir) == 0 then
     vim.fn.mkdir(undodir, "p")
 end
 
--- Setze die Einstellungen für die persistente Rückgängig-Funktion
-vim.opt.undofile = true                -- Speichert Rückgängig-Informationen in Dateien
-vim.opt.undodir = undodir              -- Verzeichnis zum Speichern der Rückgängig-Dateien
-vim.opt.undolevels = 1000              -- Anzahl der Rückgängig-Ebenen
-vim.opt.undoreload = 10000             -- Anzahl der Zeilen, die für die Rückgängig-Funktion gespeichert werden
-
+vim.opt.undofile = true
+vim.opt.undodir = undodir
+vim.opt.undolevels = 1000
+vim.opt.undoreload = 10000
